1ffe2b246229935f63535b1fa1e0944547d85ea2
ahrechushkin@ibagroup.eu <ahrechushkin@ibagroup.eu>
2021-06-16 09:52:32 +0000

    Add supports keys, values methods

--- lib/helpers/hash_with_indifferent_access_custom.rb
+++ lib/helpers/hash_with_indifferent_access_custom.rb
@@ -1,7 +1,11 @@
 class HashWithIndifferentAccessCustom
+  attr_reader :keys, :values
+  
   def initialize(hash = {})
       @attributes = Hash.new
       hash.each { |key, val| self[key] = val }
+      @keys = @attributes.keys
+      @values = @attributes.values
   end
 
   def []=(key, value)
