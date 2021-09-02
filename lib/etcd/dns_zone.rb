79ef24b1292a89b961cd8e162651084dd54dc58b
Nina Uspenskaya <nuspenskaya@ibagroup.eu>
2021-09-02 14:12:47 +0300

    dns records removal

--- lib/etcd/dns_zone.rb
+++ lib/etcd/dns_zone.rb
@@ -5,8 +5,12 @@ class CoreDns::Etcd::DnsZone < CoreDns::Etcd::Domain # CoreDns::Domain
   VALUES_WHITELIST = %w[metadata].freeze 
 
   def add(data = {})
-    data[:metadata][:zone] = true
+    #begin
+    # data[:imetadata][:zone] = true
     put(@namespace, HashWithIndifferentAccessCustom.new(data).attributes)
+   # rescue => err
+   #    "#{err}--#{data[:metadata].nil?}--#{data.inspect}"
+#    end
   end
 
   def list(level = 1)
@@ -17,9 +21,52 @@ class CoreDns::Etcd::DnsZone < CoreDns::Etcd::Domain # CoreDns::Domain
     super.select { |dns| dns.dig("metadata", "zone") }
   end
 
+  def list_subzones
+    result = []
+    fetch('').each do |record|
+     metadata_element = record.dig("metadata", "zone")
+     subzone_exists = record["hostname"].include?("/#{@client.prefix}/#{@namespace.split('.').reverse.join('/')}/") && !metadata_element.nil?  && (metadata_element == true)
+      test ="/#{@client.prefix}/#{@namespace.split('.').reverse.join('/')}/"
+     subzone_data   = record["hostname"].split('/').reverse.join('.')[0..-2]
+     result << subzone_data if subzone_exists
+    end
+    result
+  end
+
+  def list_records
+    result = []
+    one_level_records = []
+    fetch('').each do |record|
+      levels_difference = (record["hostname"].sub("/#{@client.prefix}/#{@namespace.split('.').reverse.join('/')}/",
+                                                  '')).split('/')
+      test = record["hostname"].sub("/#{@client.prefix}/#{@namespace.split('.').reverse.join('/')}/",'')
+      if levels_difference.count == 1
+        one_level_records << record
+      end
+    end
+    one_level_records.map do |record|
+     result << record unless record.dig("metadata", "zone")
+    end
+    result.map do |record|
+      hostname = record.delete("hostname")
+      record.merge!({"name" => (hostname.split('/').reverse - @namespace.split('.') - [@client.prefix]).reject!(&:empty?).join('.')})
+    end
+  end
+
   def delete(data = {})
+    hosts = []
+    results = []
+    list_records.each do |record|
+      # delete records in this zone
+      record_hostname = "#{record["name"]}.#{@namespace}./#{@client.prefix}".split('.').reverse.join('/')
+      hosts << record_hostname
+    end
     hostname = "#{@namespace}./#{@client.prefix}".split('.').reverse.join('/')
-    remove(hostname)
+    hosts << hostname
+    hosts.each do |name|
+      results << remove(name)
+    end
+    results
   end
 
   private
