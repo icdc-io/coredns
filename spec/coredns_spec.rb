943cbd4721d6ca357393837b2302dbb9215b285d
Uladzislau Zhaliazouski <uzhaliazouski@ibagroup.eu>
2022-10-18 10:56:10 +0300

    Add examples to coredns_spec.rb

--- spec/coredns_spec.rb
+++ spec/coredns_spec.rb
@@ -6,8 +6,30 @@ RSpec.describe CoreDns::Etcd do
   include_context 'shared params'
 
   describe '::new' do
-    it 'creates a connection object' do # TODO Default params ENV
-      expect(coredns.class).to eq(CoreDns::Etcd) # eq(described_class)
+    it 'creates a connection object' do
+      expect(coredns.class).to eq(described_class)
+    end
+
+    it 'creates a connection object with default params' do
+      default_params = coredns.api_url.split(':')[-1].split('/')
+      default_params << coredns.prefix << coredns.postfix
+
+      expect(default_params).to eq(['2379', 'v3', 'skydns', 'x'])
+    end
+
+    it 'creates a connection object with custom params' do
+      ENV['COREDNS_PORT']    = port    = '9732'
+      ENV['COREDNS_VERSION'] = version = 'v5'
+      ENV['COREDNS_PREFIX']  = prefix  = 'sky'
+      ENV['COREDNS_POSTFIX'] = postfix = 'y'
+
+      custom_params = coredns.api_url.split(':')[-1].split('/')
+      custom_params << coredns.prefix << coredns.postfix
+
+      ENV['COREDNS_PORT'] = ENV['COREDNS_VERSION'] =
+        ENV['COREDNS_PREFIX'] = ENV['COREDNS_POSTFIX'] = nil
+
+      expect(custom_params).to eq([port, version, prefix, postfix])
     end
   end
 end
