1f726bd55766d46035c5d2324c100440c22a1f15
Dziyana Trubelnik <dtrubelnik@ibagroup.eu>
2025-08-28 16:31:46 +0300

    fix regexp for domain, bump up coredns version

--- lib/etcd.rb
+++ lib/etcd.rb
@@ -17,7 +17,7 @@ class CoreDns::Etcd
 
   def domain(hostname)
     unless hostname.empty?
-      raise RuntimeError.new "Invalid hostname" unless hostname.match?(/\A(?:(?!-)(?!.*--)[a-zA-Z0-9\-\_]{1,63}\.)+[a-zA-Z]{2,}\z/)
+      raise RuntimeError.new "Invalid hostname" unless hostname.match?(/\A(?:(?!-)(?!.*--)[a-zA-Z0-9_][a-zA-Z0-9\-\_]{0,62}\.)+[a-zA-Z]{2,}\z/)
     end
 
     CoreDns::Etcd::Domain.new(self, hostname)
